8fc04ed1ab8a7697ec47815e5bcbfc6c682d1dc5
sagar phuge <sagarphuge@gmail.com>
2025-04-19 22:12:54 +0530

    change mongodb connection

--- lib/mongoConnect.ts
+++ lib/mongoConnect.ts
@@ -1,16 +1,29 @@
-import mongoose from "mongoose";
 
-const connectDB = async (): Promise<void> => {
-  if (mongoose.connection.readyState >= 1) {
-    console.log("Using existing MongoDB connection");
-    return;
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
   }
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-  }
-};
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectDB;
